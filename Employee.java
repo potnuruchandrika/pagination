73eb73e47d205597d999e3e1a8b3d79877995c6b
potnuruchandrika <162982661+potnuruchandrika@users.noreply.github.com>
2024-05-10 16:26:13 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,26 @@
+package com.data;
+
+public class Employee {
+	int empid;
+	String empname;
+	int empphonenumber;
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public int getEmpphonenumber() {
+		return empphonenumber;
+	}
+	public void setEmpphonenumber(int empphonenumber) {
+		this.empphonenumber = empphonenumber;
+	}
+	
+}
