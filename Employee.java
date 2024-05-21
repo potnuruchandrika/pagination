1e41bfb3436adf3eb9468b96ad105eaf3531a711
potnuruchandrika <162982661+potnuruchandrika@users.noreply.github.com>
2024-05-21 16:05:47 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,42 @@
+package com.data;
+
+public class Employee {
+	
+	String employee_Id;
+	String employee_name;
+	String phone_number;
+	String department;
+	String salary;
+	public String getEmployee_Id() {
+		return employee_Id;
+	}
+	public void setEmployee_Id(String employee_Id) {
+		this.employee_Id = employee_Id;
+	}
+	public String getEmployee_name() {
+		return employee_name;
+	}
+	public void setEmployee_name(String employee_name) {
+		this.employee_name = employee_name;
+	}
+	public String getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	
+
+}
